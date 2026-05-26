1342b7f3d93a4d81dcc864ef1be80e94374081ee
Pranaya Khadgi Shahi <pranaya.khadgi99@gmail.com>
2026-05-26 15:42:16 -0500

    added the react-router template, will build on top it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import { Routes, Route } from 'react-router';
+import { MainLayout } from './components/MainLayout';
+
+const Dashboard = () => <div>Dashboard</div>//the calender goes here later
+const Inventory = () => <div>Inventory</div>//the inventory grid goes here
+const Reservations = () => <div>Reservations</div>//the reservation table goes here
+
+
 function App() {
   return (
-    <div className="min-h-screen bg-[#fdfbf7] text-slate-800">
-      <h1 className="text=2xl font-bold text-[#1b1464] p-8">
-        Equipment Inventory System
-      </h1>
-    </div>
-  )
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/reservations" element={<Reservations />} />
+      </Route>
+    </Routes>
+  );
 }
+
+export default App;
