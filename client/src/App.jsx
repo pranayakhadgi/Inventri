511cf5a8ecd4588991ef3f02f4b139f7de21a728
Pranaya Khadgi Shahi <pranaya.khadgi99@gmail.com>
2026-05-25 23:05:04 -0500

    added vite config - unfinished

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#fdfbf7] text-slate-800">
+      <h1 className="text=2xl font-bold text-[#1b1464] p-8">
+        Equipment Inventory System
+      </h1>
+    </div>
+  )
+}
